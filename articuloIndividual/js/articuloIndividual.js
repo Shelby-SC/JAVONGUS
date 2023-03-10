b0e21c1ec3dfada8e57b2b4103e934caea881515
GamalielNB <gamalielnaranjo@gmail.com>
2023-03-09 23:32:25 -0600

    Se agregan detalles de guia de tallas y recuadro de envio, queda pendiente modificar reaccion de acordion

--- articuloIndividual/js/articuloIndividual.js
+++ articuloIndividual/js/articuloIndividual.js
@@ -1,3 +1,4 @@
+
 const COLOR_BTNS = document.querySelectorAll('.color');
 const MAIN_IMG = document.querySelector('.container.sproduct img');
 const SMALL_IMGS_CONTAINER = document.querySelector('.small-img-group');
@@ -23,8 +24,24 @@ function resetActiveBtns(){
 
 // set new color on the banner right and small images
 function setNewColor(color){
-  MAIN_IMG.src = `../img/camisa4c/1_${color}.PNG`;
+  MAIN_IMG.src = `../img/camisa4c/1_${color}.png`;
   for(let i = 0; i < 4; i++) {
-    SMALL_IMGS_CONTAINER.children[i].children[0].src = `../img/camisa4c/${i+1}_${color}.PNG`;
+    SMALL_IMGS_CONTAINER.children[i].children[0].src = `../img/camisa4c/${i+1}_${color}.png`;
   }
 }
+
+function toggleTable() {
+  var tabla = document.getElementById("tabla-tallas");
+  var boton = document.querySelector(".tallas-toggle i:last-child");
+  if (tabla.style.display === "none") {
+    tabla.style.display = "table";
+    boton.classList.remove("fa-plus");
+    boton.classList.add("fa-minus");
+  } else {
+    tabla.style.display = "none";
+    boton.classList.remove("fa-minus");
+    boton.classList.add("fa-plus");
+  }
+}
+
+// boton
